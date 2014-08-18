9b9d4efb3c28cc8251eece258adff2fde5d74a40
Matt Jennings <matt@mattjennings.net>
2014-08-17 19:53:20 -0700

    got rid of query strings when clicking buttons

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(function(){
 	function generateValidateNumber() {
 
 		// Get user number
-		var userNum = +$("#userGuess").val();
+		var userNum = $("#userGuess").val();
 		// Error or Success Message Feedback
 		var feedback = $("#feedback");
 
@@ -34,50 +34,43 @@ $(function(){
 				$("#userGuess").val("");
 				$("#count").html("0");
 
-				// Reset & generate random number from 1-100
-				var randomNum = "";
-				randomNum = Math.floor(Math.random() * (100) + 1)
-
-					// On submit error, hot, cold & success messages of user number guesses
-					$("#guessButton").on("click", function(event) {
-						event.preventDefault();
-
-						// Show answer
-						$("h1").html("ANSWER: " + randomNum);
-
-						// Error for not using a number or whole numbers not 1-100 inclusive
-	//				if(isNaN(userNum) || userNum != Math.floor(userNum) || userNum > 100 || userNum < 1) {
-	//					feedback.html("Sorry, you need to enter a whole number from 1 - 100 inclusive. Please try again.");
-	//				}
-						// Hot or cold error messages
-						if(Math.abs(randomNum - userNum) > 30) {
-							feedback.html("You're very cold");
-						}
-						else if(Math.abs(randomNum - userNum) < 30 && Math.abs(randomNum - userNum) > 15) {
-							feedback.html("You're getting warmer");
-						}
-						else if(Math.abs(randomNum - userNum) < 15 && Math.abs(randomNum - userNum) > 5) {
-							feedback.html("You're getting hotter");
-						}
-						else if(Math.abs(randomNum - userNum) < 5 && Math.abs(randomNum - userNum) > 0) {
-							feedback.html("You're very hot!");
-						}
-						else if(randomNum === userNum) {
-							feedback.html("You are correct!");
-						}
-
-
-				});
-
-
-
-
 			});
 		}
 		newGame();
 
 		function errorValidation() {
+			// On submit error, hot, cold & success messages of user number guesses
+			$("#guessButton").on("click", function(event) {
+				event.preventDefault();
 
+				// Reset & generate random number from 1-100
+				var randomNum = Math.floor(Math.random() * (100) + 1);
+
+				// Show answer
+				$("h1").html("ANSWER: " + randomNum);
+
+				// Error for not using a number or whole numbers not 1-100 inclusive
+				//				if(isNaN(userNum) || userNum != Math.floor(userNum) || userNum > 100 || userNum < 1) {
+				//					feedback.html("Sorry, you need to enter a whole number from 1 - 100 inclusive. Please try again.");
+				//				}
+				// Hot or cold error messages
+				if(Math.abs(randomNum - userNum) > 30) {
+					feedback.html("You're very cold");
+				}
+				else if(Math.abs(randomNum - userNum) < 30 && Math.abs(randomNum - userNum) > 15) {
+					feedback.html("You're getting warmer");
+				}
+				else if(Math.abs(randomNum - userNum) < 15 && Math.abs(randomNum - userNum) > 5) {
+					feedback.html("You're getting hotter");
+				}
+				else if(Math.abs(randomNum - userNum) < 5 && Math.abs(randomNum - userNum) > 0) {
+					feedback.html("You're very hot!");
+				}
+				else if(randomNum === userNum) {
+					feedback.html("You are correct!");
+				}
+
+			});
 
 		}
 		errorValidation();
