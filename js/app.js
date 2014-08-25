fb38548ce39254c909ea85bcd84e2cb270c36d19
Matt Jennings <matt@mattjennings.net>
2014-08-24 21:25:45 -0700

    done

--- js/app.js
+++ js/app.js
@@ -20,27 +20,11 @@ $(function(){
 			});
 
 
-
-		// Resets all data on page to start new game
-
-
-//			$(".new").on("click", function(event) {
-//				event.preventDefault();
-//				$("h1").html("HOT or COLD");
-//				$("#feedback").html("Make your Guess!");
-//				$("#userGuess").val("");
-//				$("#count").html("0");
-//
-//			});
-
-		// Constructor to reset game after clicking "New Game"
+		// Constructor to reset game after clicking "New Game" button
 		function NewGame(newGameBtn, min, max, mainHeading, successOrVictoryMsg, userGuess, guessCount) {
 			this.newGameBtn = newGameBtn;
 			this.min = min;
 			this.max = max;
-//			this.randomNum = function() {
-//				return Math.floor(Math.random() * (100) + 1);
-//			};
 			this.mainHeading = mainHeading;
 			this.successOrVictoryMsg = successOrVictoryMsg;
 			this.userGuess = userGuess;
@@ -48,16 +32,26 @@ $(function(){
 			this.startNewGame = function() {
 				return $(newGameBtn).on("click", function(event) {
 					event.preventDefault();
-					var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+					var randomNum = "";
+					randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
 					$(mainHeading).html("HOT or COLD");
 					$(successOrVictoryMsg).html("Make your Guess!" + randomNum);
 					$(userGuess).val("");
 					$(guessCount).html("0");
-				});
 
+					$("#guessButton").on("click", function(event) {
+						event.preventDefault();
+						$("#userGuess").val(randomNum);
+
+
+
+					});
+
+				});
 			}
 
 		}
+
 
 		// Instantiate NewGame class and then call startNewGame method from class including
 		// setting random number
@@ -68,38 +62,38 @@ $(function(){
 
 
 
-			// On submit error, hot, cold & success messages of user number guesses
-			$("#guessButton").on("click", function(event) {
-				event.preventDefault();
-
-				// Reset & generate random number from 1-100
-				//var randomNum = Math.floor(Math.random() * (100) + 1);
-
-				// Show answer
-				$("h1").html("ANSWER: " + randomNum);
-
-				// Error for not using a number or whole numbers not 1-100 inclusive
-				//				if(isNaN(userNum) || userNum != Math.floor(userNum) || userNum > 100 || userNum < 1) {
-				//					feedback.html("Sorry, you need to enter a whole number from 1 - 100 inclusive. Please try again.");
-				//				}
-				// Hot or cold error messages
-				if(Math.abs(randomNum - userNum) > 30) {
-					feedback.html("You're very cold");
-				}
-				else if(Math.abs(randomNum - userNum) < 30 && Math.abs(randomNum - userNum) > 15) {
-					feedback.html("You're getting warmer");
-				}
-				else if(Math.abs(randomNum - userNum) < 15 && Math.abs(randomNum - userNum) > 5) {
-					feedback.html("You're getting hotter");
-				}
-				else if(Math.abs(randomNum - userNum) < 5 && Math.abs(randomNum - userNum) > 0) {
-					feedback.html("You're very hot!");
-				}
-				else if(randomNum == userNum) {
-					feedback.html("You are correct!");
-				}
-
-			});
+//			// On submit error, hot, cold & success messages of user number guesses
+//			$("#guessButton").on("click", function(event) {
+//				event.preventDefault();
+//
+//				// Reset & generate random number from 1-100
+//				//var randomNum = Math.floor(Math.random() * (100) + 1);
+//
+//				// Show answer
+//				$("h1").html("ANSWER: " + randomNum);
+//
+//				// Error for not using a number or whole numbers not 1-100 inclusive
+//				//				if(isNaN(userNum) || userNum != Math.floor(userNum) || userNum > 100 || userNum < 1) {
+//				//					feedback.html("Sorry, you need to enter a whole number from 1 - 100 inclusive. Please try again.");
+//				//				}
+//				// Hot or cold error messages
+//				if(Math.abs(randomNum - userNum) > 30) {
+//					feedback.html("You're very cold");
+//				}
+//				else if(Math.abs(randomNum - userNum) < 30 && Math.abs(randomNum - userNum) > 15) {
+//					feedback.html("You're getting warmer");
+//				}
+//				else if(Math.abs(randomNum - userNum) < 15 && Math.abs(randomNum - userNum) > 5) {
+//					feedback.html("You're getting hotter");
+//				}
+//				else if(Math.abs(randomNum - userNum) < 5 && Math.abs(randomNum - userNum) > 0) {
+//					feedback.html("You're very hot!");
+//				}
+//				else if(randomNum == userNum) {
+//					feedback.html("You are correct!");
+//				}
+//
+//			});
 
 
 
