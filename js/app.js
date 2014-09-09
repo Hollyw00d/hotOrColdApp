3f81d247b5ddc704464f5050d5f369d80d98004f
Hollyw00d <matt@mattjennings.net>
2014-09-08 19:00:24 -0700

    everything done except for adding questions in code for mike h

--- js/app.js
+++ js/app.js
@@ -46,21 +46,31 @@ $(function(){
     // Reset the form back to an empty state.
     // This is just to make the application useable
     // and your implementation may be different.
+    $("#feedback").html("Make your Guess!");
+    $("#count").html("0");
     $("#guessList").html("");
     $("#guess").val("");
     $("#makeGuess").removeAttr("disabled");
     $("#guess").removeAttr("disabled");
   });
 
+// Start counter at zero to keep track of guesses until victory
+var count = 0;
+
 // Set a click listener for the Make Guess button.
 // Your implementation may be different.
   $("#makeGuess").on("click", function(event) {
     event.preventDefault();
+
     // Get the guess from the input
     var guess = $("#guess").val();
 
+
     // Using the context of the current game, call the makeGuess() function
     var result = currentGame.makeGuess(guess);
+
+
+    var currentRandomNum = currentGame.randomNum;
 
     // Build the list element
     var listElement = $("<li>").html(guess).
@@ -69,15 +79,52 @@ $(function(){
     // Add the list element
     $("#guessList").append(listElement);
 
+
+
     // Reset the form
     $("#guess").val("");
 
-    // Optionally alert and disable if the result is correct
-    if(result) {
-      alert("You Win!");
-      $("#makeGuess").attr("disabled", "disabled");
-      $("#guess").attr("disabled", "disabled");
+    // Error validation message area
+    var successOrVictoryMsgClick = $("#feedback");
+
+
+    // Count the number of guesses until success
+    var guessCountNum = $("#count");
+    if(currentRandomNum != guess) {
+      count++;
+      guessCountNum.html(count);
     }
+
+    // Form validation if the numbers 1-100 aren't entered
+    // Form submit success and error messages
+    if(isNaN(guess) || guess != Math.floor(guess) || guess > 100 || guess < 1 || guess == "") {
+      successOrVictoryMsgClick.html("Sorry, you need to enter a whole number from 1 - 100 inclusive. Please try again.");
+      // If number that is NOT 1-100 is entered then remove the blank guess which is an empty LI tag
+      if(!$.trim($("selector").html())) {
+        $(listElement).remove();
+      }
+    }
+    else if(Math.abs(currentRandomNum - guess) > 30) {
+      successOrVictoryMsgClick.html("You're very cold");
+    }
+    else if(Math.abs(currentRandomNum - guess) < 30 && Math.abs(currentRandomNum - guess) > 15) {
+      successOrVictoryMsgClick.html("You're getting warmer");
+    }
+    else if(Math.abs(currentRandomNum - guess) < 15 && Math.abs(currentRandomNum - guess) > 5) {
+      successOrVictoryMsgClick.html("You're getting hotter");
+    }
+    else if(Math.abs(currentRandomNum - currentRandomNum) < 5 && Math.abs(currentRandomNum - guess) > 0) {
+      successOrVictoryMsgClick.html("You're very hot!");
+    }
+
+    //Success message!
+    else if(Math.abs(currentRandomNum - currentRandomNum == 0)) {
+      successOrVictoryMsgClick.html("You win! The number was " + currentRandomNum + ".");
+      $("#count").html(count + 1);
+    }
+
+
+
   });
   
 });
